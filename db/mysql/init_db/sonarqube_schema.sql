46ee9b737821fc9dc6fab065469b1a346006c240
lizibin <125720240@qq.com>
2018-05-09 09:09:03 +0800

    第一次提交代码

---
+++ db/mysql/init_db/sonarqube_schema.sql
@@ -0,0 +1,5 @@
+/*
+MySQL - 5.6.30-log : Database - legendshop_docker
+*********************************************************************
+*/
+create database `legendshop_sonar` default character set utf8 collate utf8_general_ci;
