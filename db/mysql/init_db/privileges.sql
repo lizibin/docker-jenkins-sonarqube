567947220f9bdc556afedb877641b9117b9b9213
lizibin <125720240@qq.com>
2018-05-09 09:45:12 +0800

    修改部分内容

--- db/mysql/init_db/privileges.sql
+++ db/mysql/init_db/privileges.sql
@@ -3,6 +3,6 @@ select host, user from user;
 -- 因为mysql版本是5.6，因此新建用户为如下命令：
 create user sonar identified by '123456';
 -- 将docker_mysql数据库的权限授权给创建的docker用户，密码为123456：
-grant all on legendshop_sonar.* to sonar@'%' identified by '123456' with grant option;
+grant all on sonar.* to sonar@'%' identified by '123456' with grant option;
 -- 这一条命令一定要有：
 flush privileges;
--- db/mysql/init_db/sonarqube_schema.sql
+++ db/mysql/init_db/sonarqube_schema.sql
@@ -2,4 +2,4 @@
 MySQL - 5.6.30-log : Database - legendshop_docker
 *********************************************************************
 */
-create database `legendshop_sonar` default character set utf8 collate utf8_general_ci;
+create database `sonar` default character set utf8 collate utf8_general_ci;
